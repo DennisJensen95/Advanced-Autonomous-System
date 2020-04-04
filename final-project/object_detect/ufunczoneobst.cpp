64015a587e99001e7ce9b4e3b878a8aa5455dad2
theismunk <theis_mj@hotmail.com>
2020-04-04 12:41:29 +0200

    ..

--- final-project/object_detect/ufunczoneobst.cpp
+++ final-project/object_detect/ufunczoneobst.cpp
@@ -113,21 +113,23 @@ bool UFunczoneobst::handleCommand(UServerInMsg * msg, void * extra)
       vector<double> line;
       bool state = lsqline(x,y,line);
       
-      vector<double> poseR, poseW;
-      /*poseR.push_back(2.0);
-      poseR.push_back(1.0);
-      poseR.push_back(-1.57);*/
-      poseR.push_back(x);
-      poseR.push_back(y);
-      poseR.push_back(th);
+      if (state){
+	      vector<double> poseR, poseW;
+	      poseR.push_back(2.0);
+	      poseR.push_back(1.0);
+	      poseR.push_back(-1.57);
+	      //poseR.push_back(x);
+	      //poseR.push_back(y);
+	      //poseR.push_back(th);
 
-      poseW = transform(poseR);
+	      poseW = transform(poseR);
 
-      vector<double> lineW;
-      lineW = transline(line, poseW);
+	      vector<double> lineW;
+	      lineW = transline(line, poseW);
 
-      printf("Laser coordinates:\n");
-      printVec(lineW);
+	      printf("Laser coordinates:\n");
+	      printVec(lineW);
+      }
     }
   }
   else
