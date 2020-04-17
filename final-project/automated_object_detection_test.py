cf89db615a451f0b8b73c83ae4af25c9136e7ffc
Dennis Jensen <ngh1adh95@hotmail.com>
2020-04-17 14:38:57 +0200

    Kill the process

--- final-project/automated_object_detection_test.py
+++ final-project/automated_object_detection_test.py
@@ -1,20 +1,23 @@
 from subprocess import Popen, PIPE
+import select
 import time
 
 def open_ulmsserver():
     ulmsserver = Popen(['ulmsserver'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    return ulmsserver
+    out = select.poll()
+    out.register(ulmsserver.stdout, select.POLLIN)
+    return ulmsserver, out
 
 
-ulmsserver = open_ulmsserver()
+ulmsserver, out = open_ulmsserver()
 start = time.time()
 timeout = 15
 while True:
-    output = ulmsserver.stdout.readline()
-    if output == '' and ulmsserver.poll() is not None:
-        break
-    if output:
-        print(output.strip())
+    if out.poll(1):
+        print(ulmsserver.stdout.readline())
+    else:
+        print("")
+        time.sleep(0.5)
 
     if time.time() - start > timeout:
         ulmsserver.kill()
