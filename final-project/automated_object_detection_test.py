bbc84f8422c1e14de0fae6227f354ebf0e8b1363
Dennis Jensen <ngh1adh95@hotmail.com>
2020-04-17 15:26:23 +0200

    .

--- final-project/automated_object_detection_test.py
+++ final-project/automated_object_detection_test.py
@@ -1,6 +1,7 @@
 from subprocess import Popen, PIPE
-import threading
+from fcntl import fcntl, F_GETFL, F_SETFL
 import time
+from os import O_NONBLOCK, read
 
 def open_ulmsserver():
     """
@@ -8,18 +9,24 @@ def open_ulmsserver():
     :return:
     """
     ulmsserver = Popen(['ulmsserver'], stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    flags = fcntl(ulmsserver.stdout, F_GETFL)
+    fcntl(ulmsserver.stdout, F_SETFL, flags | O_NONBLOCK)
     return ulmsserver
 
 ulmsserver= open_ulmsserver()
 start = time.time()
 timeout = 15
 while True:
-    output = ulmsserver.stdout.read() # <---- hangs here
-    if not output:
+    try:
+        output = read(ulmsserver.stdout.fileno(), 1024)
+    except OSError:
         print("No more data")
         break
-    if output:
-        print(output.strip())
+
+    # if output == '' and ulmsserver.poll() is not None:
+    #     break
+    # if output:
+    #     print(output.strip())
 
     if time.time() - start > timeout:
         time.sleep(1)
