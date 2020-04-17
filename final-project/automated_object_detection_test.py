188c6e9a66fba780be30e8175fbb1f48c9202ab5
Dennis Jensen <ngh1adh95@hotmail.com>
2020-04-17 14:52:23 +0200

    .

--- final-project/automated_object_detection_test.py
+++ final-project/automated_object_detection_test.py
@@ -2,12 +2,16 @@ from subprocess import Popen, PIPE
 import select
 import time
 
+
 def open_ulmsserver():
-    ulmsserver = Popen(['ulmsserver'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    """
+    Open ulmserver
+    :return:
+    """
+    ulmsserver = Popen(['ulmsserver'], stdout=PIPE, stdin=PIPE, universal_newlines=True)
     out = select.poll()
     out.register(ulmsserver.stdout, select.POLLIN)
     return ulmsserver, out
-
 
 ulmsserver, out = open_ulmsserver()
 start = time.time()
