1770af809e29207bbaedff29a5998315fa5d6d58
Dennis Jensen <ngh1adh95@hotmail.com>
2020-04-17 14:56:01 +0200

    .

--- final-project/automated_object_detection_test.py
+++ final-project/automated_object_detection_test.py
@@ -8,7 +8,7 @@ def open_ulmsserver():
     Open ulmserver
     :return:
     """
-    ulmsserver = Popen(['ulmsserver'], stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    ulmsserver = Popen(['ulmsserver'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
     out = select.poll()
     out.register(ulmsserver.stdout, select.POLLIN)
     return ulmsserver, out
@@ -24,7 +24,7 @@ while True:
         time.sleep(0.5)
 
     if time.time() - start > timeout:
-        ulmsserver.stdin.write('q')
+        # ulmsserver.stdin.write('q')
         time.sleep(1)
         ulmsserver.terminate()
         break
